6de4d065b51114e51ec847f068baa1ca4072cb42
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-02-15 18:29:34 +0900

    ArticleControllerに投稿処理追加

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.Article;
+import com.example.form.ArticleForm;
 import com.example.service.ArticleService;
 
 /**
@@ -21,11 +23,33 @@ public class ArticleController {
 	@Autowired
 	private ArticleService articleService;
 	
+	@ModelAttribute
+	public ArticleForm setUpArticleForm() {
+		return new ArticleForm();
+	}
+	
 	@RequestMapping("")
 	public String index(Model model) {
 		List<Article> articleList = articleService.findAll();
 		model.addAttribute("articleList", articleList);
 		
 		return "bbs";
+	}
+	
+	@RequestMapping("/post")
+	public String post(ArticleForm form, Model model) {
+		Article article = new Article();
+		/*test
+		form.setName("user1");
+		form.setContent("ahhhh");
+		Article article = new Article();
+		article.setName("user");
+		article.setContent("hello World");
+		*/
+		article.setName(form.getName());
+		article.setContent(form.getContent());
+		articleService.insert(article);
+		
+		return index(model);
 	}
 }
---
+++ src/main/java/com/example/form/ArticleForm.java
@@ -0,0 +1,26 @@
+package com.example.form;
+
+/**
+ * 
+ * @author hondayuki
+ *
+ */
+public class ArticleForm {
+	/** 名前 */
+	private String name;
+	/** 内容 */
+	private String content;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
