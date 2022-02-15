77d244b180da53605c74bb04bebbd74da607b7c4
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-02-15 18:17:43 +0900

    articleのinsert()を作成

--- src/main/java/com/example/repository/ArticleRepository.java
+++ src/main/java/com/example/repository/ArticleRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import com.example.domain.Article;
@@ -27,5 +29,15 @@ public class ArticleRepository {
 		List<Article> articleList = template.query(sql, ARTICLE_ROW_MAPPER);
 		
 		return articleList;
+	}
+	
+	public void insert(Article article) {
+		String sql = "insert into articles"
+				+ "       ( name,  content)"
+				+ " values(:name, :content)";
+		SqlParameterSource param = new MapSqlParameterSource()
+				.addValue("name", article.getName()).addValue("content", article.getContent());
+		
+		template.update(sql, param);
 	}
 }
