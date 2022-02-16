524d9d5bed053a562957a5688b82df0541faa48f
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-02-16 09:35:28 +0900

    commentのservice&repositoryにfindByArticleId()を追加

--- src/main/java/com/example/service/CommentService.java
+++ src/main/java/com/example/service/CommentService.java
@@ -1,6 +1,12 @@
 package com.example.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.domain.Comment;
+import com.example.repository.CommentRepository;
 
 /**
  * 
@@ -9,5 +15,10 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class CommentService {
-
+	@Autowired
+	private CommentRepository commentRepository;
+	
+	public List<Comment> findByArticleId(int articleId) {
+		return commentRepository.findByArticleId(articleId);
+	}
 }
